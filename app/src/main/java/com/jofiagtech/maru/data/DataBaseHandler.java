8b062841ffe99c8ebb834f8f94412eb8676f6320
Jofiag <Aguemon.wahab.97@gmail.com>
2020-01-03 10:15:37 +0100

    Creating constants class.

--- app/src/main/java/com/jofiagtech/maru/data/DataBaseHandler.java
+++ app/src/main/java/com/jofiagtech/maru/data/DataBaseHandler.java
@@ -1,10 +1,18 @@
 package com.jofiagtech.maru.data;
 
+import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 /*public class DataBaseHandler extends SQLiteOpenHelper
 {
+    private final Context mContext;
+
+    public DataBaseHandler(Context context)
+    {
+        mContext = context;
+    }
+
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase)
     {
---
+++ app/src/main/java/com/jofiagtech/maru/util/Constants.java
@@ -0,0 +1,18 @@
+package com.jofiagtech.maru.util;
+
+import android.provider.BaseColumns;
+
+public class Constants implements BaseColumns
+{
+    public static final int DB_VERSION = 1;
+    public static final String DB_NAME = "meetingList";
+    public static final String TABLE_NAME = "meeting_table";
+
+    //Table columns
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_SUBJECT = "subject";
+    public static final String COLUMN_TIME = "time";
+    public static final String COLUMN_PLACE = "place";
+    public static final String COLUMN_NUMBER_OF_PARTICIPANT = "number_of_participant";
+
+}
