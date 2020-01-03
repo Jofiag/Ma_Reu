e7464243c4a87b8484e114d800cef537d65cc0fb
Jofiag <Aguemon.wahab.97@gmail.com>
2020-01-03 14:27:13 +0100

    Finish list row layout and recycler view

--- app/src/main/java/com/jofiagtech/maru/adapter/RecyclerViewAdapter.java
+++ app/src/main/java/com/jofiagtech/maru/adapter/RecyclerViewAdapter.java
@@ -5,12 +5,15 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.jofiagtech.maru.R;
 import com.jofiagtech.maru.model.Meeting;
+import com.jofiagtech.maru.model.Participant;
 
 import java.util.List;
 
@@ -18,10 +21,12 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 {
     private Context mContext;
     private List<Meeting> mMeetingList;
+    private List<Participant> mParticipantList;
 
-    public RecyclerViewAdapter(Context context, List<Meeting> meetingList) {
+    public RecyclerViewAdapter(Context context, List<Meeting> meetingList, List<Participant> participantList) {
         mContext = context;
         mMeetingList = meetingList;
+        mParticipantList = participantList;
     }
 
     @NonNull
@@ -36,7 +41,19 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        Meeting meeting = new Meeting();
+        Meeting meeting = mMeetingList.get(position);
+
+        StringBuilder email = new StringBuilder();
+
+        for (Participant participant : mParticipantList){
+            email.append(participant.getEmail()).append(", ");
+        }
+
+        holder.subject.setText(meeting.getSubject());
+        holder.time.setText(meeting.getTime());
+        holder.place.setText(meeting.getPlace());
+        holder.participantEmail.setText(email);
+        holder.date.setText(meeting.getDate());
 
     }
 
@@ -48,9 +65,32 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
     public class ViewHolder extends RecyclerView.ViewHolder
     {
+        public TextView subject;
+        public TextView time;
+        public TextView place;
+        public TextView participantEmail;
+        public TextView date;
+        public Button deleteButton;
+
         public ViewHolder(@NonNull View itemView)
         {
             super(itemView);
+
+            subject = itemView.findViewById(R.id.mlr_subject);
+            time = itemView.findViewById(R.id.mlr_time);
+            place = itemView.findViewById(R.id.mlr_place);
+            participantEmail = itemView.findViewById(R.id.mlr_participant_email);
+            date = itemView.findViewById(R.id.mlr_participant_date);
+            deleteButton = itemView.findViewById(R.id.delete_button);
+
+            deleteButton.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View view)
+                {
+
+                }
+            });
         }
     }
 }
